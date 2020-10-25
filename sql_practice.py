8fd9bf767db30ebbc9984e8d1842a30f3ad7261c
pshepshe <71153364+pshepshe@users.noreply.github.com>
2020-10-25 05:03:46 +0300

    added rest tables and created connection between them

--- sql_practice.py
+++ sql_practice.py
@@ -4,7 +4,15 @@ import sqlite3
 
 db_conn = sqlite3.connect('pupil.db')
 cursor = db_conn.cursor()
-a = 'dsd'
+
+try:
+    cursor.execute("""CREATE TABLE subjects
+                        (subject_id TEXT PRIMARY KEY,
+                        teacher_name TEXT);
+                        """)
+except sqlite3.Error:
+    print('+')
+
 try:
     cursor.execute("""CREATE TABLE students 
                 (student_id INTEGER PRIMARY KEY, 
@@ -12,33 +20,62 @@ try:
                 second_name TEXT, 
                 country TEXT,    
                 group_number TEXT, 
-                birthday TEXT)
+                birthday INTEGER);
                 """)
 except sqlite3.Error:
     print('+')
+
 try:
     cursor.execute("""CREATE TABLE marks
-                   (mark_id TEXT PRIMARY KEY,
-                   subject_name TEXT,
-                   mark TEXT, 
-                   time TEXT
-                   """)
+                (mark_id TEXT PRIMARY KEY,
+                subject_id,
+                student_id,
+                mark TEXT,
+                time INTEGER,
+                FOREIGN KEY (student_id) 
+                    REFERENCES students(student_id),
+                FOREIGN KEY (subject_id)
+                    REFERENCES subjects(subject_id));
+                """)
 except sqlite3.Error:
     print('+')
-student_id = 0
+
+
+with open('subject_list.csv', 'r') as text:
+    line = text.readline()
+    while line != '':
+        line = line.split(',')
+        line.pop()
+        try:
+            cursor.execute("INSERT INTO subjects(subject_id, teacher_name) "
+                           "VALUES(?, ?)", line)
+        except sqlite3.Error:
+            print('-')
+        line = text.readline()
+    db_conn.commit()
+
 with open('pupil_list.csv', 'r') as text:
     line = text.readline()
     while line != '':
-        student_id += 1
         line = line.split(',')
-        line.insert(0, student_id)
         line.pop()
         try:
             cursor.execute("INSERT INTO students(student_id, first_name, second_name, country, group_number, birthday) "
-                           "VALUES(?, ?, ?, ?, ?, ?)", line)
+                           "VALUES(?, ?, ?, ?, ?, strftime('%s', ?))", line)
+        except sqlite3.Error:
+            print('1')
+        line = text.readline()
+    db_conn.commit()
+
+with open('mark_list.csv', 'r') as text:
+    line = text.readline()
+    while line != '':
+        line = line.split(',')
+        line.pop()
+        try:
+            cursor.execute("INSERT INTO marks(mark_id, subject_id, student_id, mark, time) "
+                           "VALUES(?, ?, ?, ?, strftime('%s', ?))", line)
         except sqlite3.Error:
             print()
         line = text.readline()
     db_conn.commit()
-
-
